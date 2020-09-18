27d369b2c3d9284bfa6fa76d01db74e468767fcb
Bence Fieszl <bence.fieszl@gmail.com>
2020-09-18 20:53:14 +0200

    Argument kezelő try-catch kivételkezelése hibás input esetén.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "Arena.h"
-#include<stdlib.h>
+
 
 
 using namespace std;
@@ -10,9 +10,15 @@ int main(int argc, char *argv[])
     if(argc==7)
     {
     Arena* Fight = new Arena();
+    try{
     Fight->addHero(Hero(argv[1],stoi(argv[2]),stoi(argv[3])));
     Fight->addHero(Hero(argv[4],stoi(argv[5]),stoi(argv[6])));
     cout<<Fight->Fight()<<endl;
+    }
+    catch(const std::invalid_argument& ia)
+    {
+        	  std::cerr << "Invalid argument: " << ia.what() << ". Please give the correct arguments. \n";
+    }
     delete Fight;
     return 0;
     }
