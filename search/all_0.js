05a88aa031a4a8b069864dde760441d247d9e466
97Vix <56716721+97Vix@users.noreply.github.com>
2020-11-08 16:52:09 +0000

    Update gh-pages to output generated at 9f8540b

--- search/all_0.js
+++ search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['addxp',['addXP',['../classHero.html#a94c02bccb6e2177217eba8e68cbd60d2',1,'Hero']]],
   ['attack',['Attack',['../classCharacter.html#aa62e72ce7f0beb300c206b288b1c8db1',1,'Character']]]
 ];
